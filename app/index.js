76136188630f97c9f747174c8997c6594bb3590c
Tomislav Tipurić <totipu@nephos.eu>
2021-12-06 21:34:33 +0100

    Changed to ATD16 demo. Configured platform writing

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
+const { stringify } = require('querystring');
 
 const app = express();
 app.use(morgan('combined'));
 
-
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  var content = fs.readFileSync(__dirname + '/index.html');
+  
+  content = content.toString().replace('###REPLACE_PLATFORM_NAME###', process.env.AZSVC);
+
+  res.send(content);
 });
 
 var listener = app.listen(process.env.PORT || 80, function() {
