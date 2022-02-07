449cc9e4d3738b328e4381185e0eb3e592290458
Tomislav Tipurić <totipu@nephos.eu>
2022-02-07 21:35:35 +0100

    Added logging response time

--- app/index.js
+++ app/index.js
@@ -2,19 +2,26 @@ const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
 const { stringify } = require('querystring');
+const responseTime = require('response-time')
 
 const app = express();
+
+// Measure the time of response
+app.use(responseTime((req, res, time) => {
+  console.log(`${req.method} ${req.url} Request finished in: ${time.toFixed(5)} ms`);
+}))
+
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   var content = fs.readFileSync(__dirname + '/index.html');
-  
+
   content = content.toString().replace('###REPLACE_PLATFORM_NAME###', process.env.AZSVC);
 
   res.send(content);
 });
 
 var listener = app.listen(process.env.PORT || 80, function() {
- console.log('listening on port ' + listener.address().port);
- console.log('running on ' + process.env.AZSVC);
+  console.log('listening on port ' + listener.address().port);
+  console.log('running on ' + process.env.AZSVC);
 });
